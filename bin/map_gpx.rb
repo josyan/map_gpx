5008eff10d406c2a66e29c092944af7a80bae1e5
Josyan Pierson <josyan.pierson@gmail.com>
2013-02-15 11:27:18 -0500

    Colorize gps track depending on speed

--- bin/map_gpx.rb
+++ bin/map_gpx.rb
@@ -6,15 +6,17 @@ require 'fileutils'
 require 'rasem'
 require 'pp'
 
-FACTOR = 30000
+DIST_FACTOR = 10000
+SPEED_FACTOR = 100
 
 class Point
-  attr_accessor :lat, :lon, :at
+  attr_accessor :lat, :lon, :at, :speed
 
   def initialize(lat, lon, at)
     @lat = lat.to_f
     @lon = lon.to_f
-    @at = DateTime.parse(at)
+    @at = DateTime.parse(at).to_time
+    @speed = 0
   end
 end
 
@@ -50,13 +52,13 @@ end
 def translate(min_max, points)
   min_lat = min_max[:lat][:min]
   min_max[:lat][:min] = 0
-  min_max[:lat][:max] = (min_max[:lat][:max] - min_lat) * FACTOR
+  min_max[:lat][:max] = (min_max[:lat][:max] - min_lat) * DIST_FACTOR
   min_lon = min_max[:lon][:min]
   min_max[:lon][:min] = 0
-  min_max[:lon][:max] = (min_max[:lon][:max] - min_lon) * FACTOR
+  min_max[:lon][:max] = (min_max[:lon][:max] - min_lon) * DIST_FACTOR
   points.each do |point|
-    point.lat = (point.lat - min_lat) * FACTOR
-    point.lon = (point.lon - min_lon) * FACTOR
+    point.lat = (point.lat - min_lat) * DIST_FACTOR
+    point.lon = (point.lon - min_lon) * DIST_FACTOR
   end
   # invert all lat, else drawing is top <-> bottom
   max_lat = 0
@@ -68,13 +70,46 @@ def translate(min_max, points)
   end
 end
 
+def compute_speed(points)
+  points.each_index do |index|
+    if index > 0
+      dist = Math.sqrt((points[index - 1].lon - points[index].lon)**2 + (points[index - 1].lat - points[index].lat)**2)
+      time = points[index].at - points[index - 1].at
+      points[index].speed = dist / time * SPEED_FACTOR if time > 0
+    end
+  end
+end
+
+def get_min_max_speed(min_max, points)
+  min_max[:speed] ||= {}
+  points.each_with_index do |point, index|
+    if index == 0
+      min_max[:speed][:min] = point.speed
+      min_max[:speed][:max] = point.speed
+    else
+      min_max[:speed][:min] = point.speed if point.speed < min_max[:speed][:min]
+      min_max[:speed][:max] = point.speed if point.speed > min_max[:speed][:max]
+    end
+  end
+  min_max
+end
+
+def generate_speed_color(min_speed, max_speed, current_speed)
+  percent = (current_speed - min_speed) / (max_speed - min_speed)
+  if percent < 0.5
+    "rgb(255,#{(255 * percent * 2).round},0)"
+  else
+    "rgb(#{(255 * (1 - percent * 2)).round},255,0)"
+  end
+end
+
 def generate_svg(filename, points, min_max)
   FileUtils.mkdir_p('svg')
   File.open("svg/#{File.basename(filename, '.gpx')}.svg", 'w') do |svg_file|
     Rasem::SVGImage.new(min_max[:lon][:max], min_max[:lat][:max], svg_file) do |image|
       points.each_index do |index|
         if index > 0
-          line points[index - 1].lon, points[index - 1].lat, points[index].lon, points[index].lat, 'stroke' => 'black', 'stroke-width' => 3
+          line points[index - 1].lon, points[index - 1].lat, points[index].lon, points[index].lat, 'stroke' => generate_speed_color(min_max[:speed][:min], min_max[:speed][:max], points[index].speed), 'stroke-width' => 3 
         end
       end
     end
@@ -88,5 +123,7 @@ Dir['gpx/*.gpx'].each do |gpx_file|
   points = parse_gpx(gpx_file)
   min_max = get_min_max(points)
   translate(min_max, points)
+  compute_speed(points)
+  min_max = get_min_max_speed(min_max, points)
   generate_svg(File.basename(gpx_file), points, min_max)
 end
