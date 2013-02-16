ab3a0fa84d8a68895ae3fa1d990c3c6b2eea076d
Josyan Pierson <josyan.pierson@gmail.com>
2013-02-16 14:39:59 -0500

    Some heavy refactoring, code broken

--- bin/map_gpx.rb
+++ bin/map_gpx.rb
@@ -9,27 +9,58 @@ require 'pp'
 DIST_FACTOR = 10000
 
 class Point
-  attr_accessor :lat, :lon, :at, :speed
+  attr_accessor :x, :y, :at, :speed
 
-  def initialize(lat, lon, at)
-    @lat = lat.to_f
-    @lon = lon.to_f
+  def initialize(x, y, at)
+    @x = x.to_f
+    @y = y.to_f
     @at = DateTime.parse(at).to_time
     @speed = 0
+  end
+
+  def speed_color(min_speed, max_speed)
+    percent = (@speed - min_speed) / (max_speed - min_speed)
+    if percent < 0.5
+      "rgb(255,#{(255 * percent * 2).round},0)"
+    else
+      "rgb(#{(255 * (1 - percent * 2)).round},255,0)"
+    end
   end
 end
 
 class Track
   SPEED_FACTOR = 100
 
-  attr_accessor :filename, :points, :min_speed, :max_speed, :min_lat, :max_lat, :min_lon, :max_lon
+  attr_accessor :filename, :points, :min_speed, :max_speed, :min_x, :max_x, :min_y, :max_y
 
   def initialize(gpx_file)
     @filename = gpx_file
+    puts "  Parsing #{gpx_file}"
     @points = parse_gpx(@filename)
+    puts "  Computing track limits"
     set_min_max_pos
+    puts "  Computing speed"
     compute_speed
-    set_min_map_speed
+    puts "  Computing speed limits"
+    set_min_max_speed
+  end
+
+  def translate(min_x, min_y)
+    @min_x = (@min_x - min_x) * DIST_FACTOR
+    @max_x = (@max_x - min_x) * DIST_FACTOR
+    @min_y = (@min_y - min_y) * DIST_FACTOR
+    @max_y = (@max_y - min_y) * DIST_FACTOR
+    @points.each do |point|
+      point.x = (point.x - min_x) * DIST_FACTOR
+      point.y = (point.y - min_y) * DIST_FACTOR
+    end
+  end
+
+  def invert_y
+    amp = @max_y - @min_y
+    @points.each do |point|
+      point.y = @min_y + amp - point.y
+    end
   end
 
   private
@@ -38,23 +69,23 @@ class Track
     gpx_doc = Nokogiri::XML.parse(File.open(gpx_file))
     gpx_doc.remove_namespaces!
     gpx_doc.xpath('/gpx/trk/trkseg/trkpt').map do |gpx_point|
-      Point.new(gpx_point.attribute('lat').content, gpx_point.attribute('lon').content, gpx_point.at_xpath('time').content)
+      Point.new(gpx_point.attribute('lon').content, gpx_point.attribute('lat').content, gpx_point.at_xpath('time').content)
     end
   end
 
   def set_min_max_pos
     @points.each do |point|
-      @min_lat = point.lat if @min_lat.nil? or point.lat < @min_lat
-      @max_lat = point.lat if @max_lat.nil? or point.lat > @max_lat
-      @min_lon = point.lon if @min_lon.nil? or point.lon < @min_lon
-      @max_lon = point.lon if @max_lon.nil? or point.lon > @max_lon
+      @min_x = point.x if @min_x.nil? or point.x < @min_x
+      @max_x = point.x if @max_x.nil? or point.x > @max_x
+      @min_y = point.y if @min_y.nil? or point.y < @min_y
+      @max_y = point.y if @max_y.nil? or point.y > @max_y
     end
   end
 
   def compute_speed
     @points.each_index do |index|
       if index > 0
-        dist = Math.sqrt((points[index - 1].lon - points[index].lon)**2 + (points[index - 1].lat - points[index].lat)**2)
+        dist = Math.sqrt((points[index - 1].x - points[index].x)**2 + (points[index - 1].y - points[index].y)**2)
         time = points[index].at - points[index - 1].at
         points[index].speed = dist / time * SPEED_FACTOR if time > 0
       end
@@ -69,66 +100,51 @@ class Track
   end
 end
 
-def get_min_max(min_max, tracks)
+def get_min_max_pos(min_max, tracks)
   tracks.each do |track|
-    min_max[:lat][:min] = track.min_lat if min_max[:lat][:min].nil? or track.min_lat < min_max[:lat][:min]
-    min_max[:lat][:max] = track.max_lat if min_max[:lat][:max].nil? or track.max_lat > min_max[:lat][:max]
-    min_max[:lon][:min] = track.min_lon if min_max[:lon][:min].nil? or track.min_lon < min_max[:lon][:min]
-    min_max[:lon][:max] = track.max_lon if min_max[:lon][:max].nil? or track.max_lon > min_max[:lon][:max]
+    min_max[:min_x] = track.min_x if min_max[:min_x].nil? or track.min_x < min_max[:min_x]
+    min_max[:max_x] = track.max_x if min_max[:max_x].nil? or track.max_x > min_max[:max_x]
+    min_max[:min_y] = track.min_y if min_max[:min_y].nil? or track.min_y < min_max[:min_y]
+    min_max[:max_y] = track.max_y if min_max[:max_y].nil? or track.max_y > min_max[:max_y]
   end
 end
 
 def translate(min_max, tracks)
-  min_lat = min_max[:lat][:min]
-  min_max[:lat][:min] = 0
-  min_max[:lat][:max] = (min_max[:lat][:max] - min_lat) * DIST_FACTOR
-  min_lon = min_max[:lon][:min]
-  min_max[:lon][:min] = 0
-  min_max[:lon][:max] = (min_max[:lon][:max] - min_lon) * DIST_FACTOR
+  min_x = min_max[:min_x]
+  min_max[:min_x] = 0
+  min_max[:max_x] = (min_max[:max_x] - min_x) * DIST_FACTOR
+  min_y = min_max[:min_y]
+  min_max[:min_y] = 0
+  min_max[:max_y] = (min_max[:max_y] - min_y) * DIST_FACTOR
   tracks.each do |track|
-    track[:points].each do |point|
-      point.lat = (point.lat - min_lat) * DIST_FACTOR
-      point.lon = (point.lon - min_lon) * DIST_FACTOR
-    end
+    track.translate min_x, min_y
   end
-  # invert all lat, else drawing is top <-> bottom
-  max_lat = 0
+  # invert all y, else drawing is top <-> bottom
   tracks.each do |track|
-    track[:points].each do |point|
-      max_lat = point.lat if point.lat > max_lat
-    end
-  end
-  tracks.each do |track|
-    track[:points].each do |point|
-      point.lat = max_lat - point.lat
-    end
+    track.invert_y
   end
 end
 
-def get_min_max_speed(min_max, points)
-  points.each do |point|
-    min_max[:speed][:min] = point.speed if min_max[:speed][:min].nil? or point.speed < min_max[:speed][:min]
-    min_max[:speed][:max] = point.speed if min_max[:speed][:max].nil? or point.speed > min_max[:speed][:max]
+def get_min_max_speed(min_max, tracks)
+  tracks.each do |track|
+    min_max[:min_speed] = track.min_speed if min_max[:min_speed].nil? or track.min_speed < min_max[:min_speed]
+    min_max[:max_speed] = track.max_speed if min_max[:max_speed].nil? or track.max_speed > min_max[:max_speed]
   end
 end
 
-def generate_speed_color(min_speed, max_speed, current_speed)
-  percent = (current_speed - min_speed) / (max_speed - min_speed)
-  if percent < 0.5
-    "rgb(255,#{(255 * percent * 2).round},0)"
-  else
-    "rgb(#{(255 * (1 - percent * 2)).round},255,0)"
-  end
-end
-
-def generate_svg(tracks, min_max)
+def generate_svg(min_max, tracks)
   FileUtils.mkdir_p('svg')
-  File.open("svg/graph_#{Time.now.strftime('%Y%m%d%H%M%S')}.svg", 'w') do |svg_file|
-    Rasem::SVGImage.new(min_max[:lon][:max].round(2), min_max[:lat][:max].round(2), svg_file) do |image|
+  # File.open("svg/graph_#{Time.now.strftime('%Y%m%d%H%M%S')}.svg", 'w') do |svg_file|
+  File.open("svg/graph_test.svg", 'w') do |svg_file|
+    Rasem::SVGImage.new(min_max[:max_x].round(2), min_max[:max_y].round(2), svg_file) do |image|
       tracks.each do |track|
-        track[:points].each_index do |index|
+        track.points.each_index do |index|
           if index > 0
-            line track[:points][index - 1].lon.round(2), track[:points][index - 1].lat.round(2), track[:points][index].lon.round(2), track[:points][index].lat.round(2), 'stroke' => generate_speed_color(min_max[:speed][:min], min_max[:speed][:max], track[:points][index].speed), 'stroke-width' => 3, 'stroke-opacity' => 0.1
+            line track.points[index - 1].x.round(2), track.points[index - 1].y.round(2),
+                 track.points[index].x.round(2), track.points[index].y.round(2),
+                 'stroke' => track.points[index].speed_color(track.min_speed, track.max_speed),
+                 'stroke-width' => 1,
+                 'stroke-opacity' => (1.0 / tracks.length)
           end
         end
       end
@@ -140,30 +156,26 @@ puts "Mapping GPX files to SVG"
 
 tracks = []
 
+puts "Parsing files"
 Dir['gpx/*.gpx'].each do |gpx_file|
-  puts "Parsing #{gpx_file}..."
   tracks << Track.new(gpx_file)
 end
 
-min_max = { :lat   => { :min => nil,
-                         :max => nil },
-             :lon   => { :min => nil,
-                         :max => nil },
-             :speed => { :min => nil,
-                         :max => nil } }
-puts "Computing boundaries..."
-get_min_max min_max, tracks
+min_max = { :min_x => nil,
+            :max_x => nil,
+            :min_y => nil,
+            :max_y => nil,
+            :min_speed => nil,
+            :max_speed => nil }
 
-puts "Normalizing..."
+puts "Computing drawing limits"
+get_min_max_pos min_max, tracks
+
+puts "Normalizing"
 translate min_max, tracks
 
-puts "Computing speeds..."
-tracks.each do |track|
-  compute_speed(track[:points])
-end
-puts "Computing speed limits..."
-tracks.each do |track|
-  get_min_max_speed(min_max, track[:points])
-end
-puts "Rendering..."
-generate_svg(tracks, min_max)
+puts "Computing speed limits"
+get_min_max_speed min_max, tracks
+
+puts "Rendering"
+generate_svg min_max, tracks
